134989f53a7b6169c9e54b667b4fecca8f14a0b1
jse <jsefao011>
2018-12-17 15:40:41 -0500

    Insert KK

--- crmedatabase/src/main/java/com/consultoraestrategia/ss_crmeducativo/database/crmedatabase/Dbas.java
+++ crmedatabase/src/main/java/com/consultoraestrategia/ss_crmeducativo/database/crmedatabase/Dbas.java
@@ -2,6 +2,7 @@ package com.consultoraestrategia.ss_crmeducativo.database.crmedatabase;
 
 public class Dbas {
     private int Jose;
+    private String kk;
 
     public int getJose() {
         return Jose;
@@ -9,5 +10,13 @@ public class Dbas {
 
     public void setJose(int jose) {
         Jose = jose;
+    }
+
+    public String getKk() {
+        return kk;
+    }
+
+    public void setKk(String kk) {
+        this.kk = kk;
     }
 }
