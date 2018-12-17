c7b5371110473bb458bfd598931f32334889cbfc
jse <jsefao011>
2018-12-17 15:19:06 -0500

    Agregar Dbas

--- crmedatabase/src/main/java/com/consultoraestrategia/ss_crmeducativo/database/crmedatabase/Dbas.java
+++ crmedatabase/src/main/java/com/consultoraestrategia/ss_crmeducativo/database/crmedatabase/Dbas.java
@@ -1,4 +1,13 @@
 package com.consultoraestrategia.ss_crmeducativo.database.crmedatabase;
 
 public class Dbas {
+    private int Jose;
+
+    public int getJose() {
+        return Jose;
+    }
+
+    public void setJose(int jose) {
+        Jose = jose;
+    }
 }
